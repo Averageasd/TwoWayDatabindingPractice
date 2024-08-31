a3206044d9d9f76e9af90430a6f0a34383120132
Averageasd <nhuynh9497@gmail.com>
2024-08-31 17:22:03 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'TwoWayBinding';
+export class AppComponent implements OnInit {
+  title: string = 'TwoWayBinding';
+  username: string = '';
+  constructor() {}
+  ngOnInit(): void {}
+
+  onCanReset() {
+    return this.username.trim().length === 0;
+  }
+
+  onResetForm() {
+    this.username = '';
+  }
 }
